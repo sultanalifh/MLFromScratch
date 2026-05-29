52e46c8f3863a47bc8c64048299017e38c5c0062
sultanalifh <sultanalif533@gmail.com>
2026-05-29 08:57:25 +0700

    Data Augmentation

--- Core/ImageVisualizer.cs
+++ Core/ImageVisualizer.cs
@@ -65,4 +65,87 @@ static class ImageVisualizer
 
         return result;
     }
+    public static double[] Scale(double[] image, int width, int scale)
+    {
+        int n = image.Length;
+        
+        int cx = width / 2;
+        int cy = width / 2;
+
+        double[] result = new double[n];
+
+        for(int y = 0; y < width; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                double srcX = (x - cx) / scale + cx;
+                double srcY = (y - cy) / scale + cy;
+
+                result[y * width + x] = BiliniarInterpolation(image, width, srcX, srcY);
+            }
+        }
+
+        return result;
+    }
+
+    public static double[] Rotate(double[] image, int width, double theta)
+    {
+        int n = image.Length;
+
+        int cx = width / 2;
+        int cy = width / 2;
+
+        double[] result = new double[n];
+
+        for(int y = 0; y < width; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                int dx = x - cx;
+                int dy = y - cy;
+
+                double srcX = Math.Cos(theta) * dx + Math.Sin(theta) * dy + cx;
+                double srcY = -Math.Sin(theta) * dx + Math.Cos(theta) * dy + cy;
+
+                result[y * width + x] = BiliniarInterpolation(image, width, srcX, srcY);
+            }
+        }
+
+        return result;
+    }
+
+    public static double BiliniarInterpolation(double[] image, int width, double x, double y)
+    {
+        int x0 = (int) x;
+        int y0 = (int) y;
+
+        int x1 = x0 + 1;
+        int y1 = y0 + 1;
+
+        double tx = x - x0;
+        double ty = y - y0;
+
+        double ldy0, ldy1, rdy0, rdy1;
+
+        ldy0 = ldy1 = rdy0 = rdy1 = 0;
+
+        if(y0 >= 0 && y0 < width)
+        {
+            ldy0 = x0 >= 0 && x0 < width ? image[y0 * width + x0] : 0;
+            ldy1 = x1 >= 0 && x1 < width ? image[y0 * width + x1] : 0;
+        }
+
+        if(y1 >= 0 && y1 < width)
+        {
+            rdy0 = x0 >= 0 && x0 < width ? image[y1 * width + x0] : 0;
+            rdy1 = x1 >= 0 && x1 < width ? image[y1 * width + x1] : 0;
+        }
+
+        double ldy = ldy0 * (1 - tx) + ldy1 * tx;
+        double rdy = rdy0 * (1 - tx) + rdy1 * tx;
+
+        double result = ldy * (1 - ty) * rdy * ty;
+
+        return result;
+    }
 }
